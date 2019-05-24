d10e08448ad6f34ef07583c5322f05ce8c59c978
Manan Rai <mananrai@stanford.edu>
2019-05-23 17:54:15 -0700

    Classical (i)qft for reference

--- qft.py
+++ qft.py
@@ -4,6 +4,27 @@
 		2. Inverse Fourier Transform
 '''
 
+import numpy as np
+
+########################################
+# For Reference
+########################################
+
+def classical_qft(j, d):
+	k_vec = lambda k : np.array([0 for i in range(d) if i != k else 1])
+	omega = 2.0 * np.pi * 1j / d
+	return (1.0 / np.sqrt(d)) * np.sum([omega ** (k * j) * k_vec(k) for k in range(d)])
+
+def classical_iqft(j, d):
+	k_vec = lambda k : np.array([0 for i in range(d) if i != k else 1])
+	omega = 2.0 * np.pi * 1j / d
+	return (1.0 / np.sqrt(d)) * np.sum([omega ** (-k * j) * k_vec(k) for k in range(d)])
+
+
+########################################
+# Quantum Implemenations
+########################################
+
 def qft(j, d):
 	pass
 
