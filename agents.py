de29bb5a1a8a2cb02906f7388a9637da3510a63c
Andrew-Tierno <andrew.tierno@gmail.com>
2019-06-05 21:18:04 -0700

    Reworked Alice

--- agents.py
+++ agents.py
@@ -4,66 +4,112 @@ from pyquil import Program
 from pyquil.gates import RX, RY, RZ, X, Y, Z, H, CNOT
 from random import random
 from qft import qft, iqft
-import math
+from entangle import init_p, entangle, disentangle
 import numpy as np
 
 Q = Union[int, QubitPlaceholder]
-QubitMap = List[List[Q]]
-QubitSourceMap = List[List[Tuple[Q, int]]]
+QubitShare = Tuple[Q, int]
+QubitShareList = List[QubitShare]
+QubitShareMatrix = List[QubitShareList]
 
 class Alice:
-    def __init__(self, secret_ansatz: Callable[[List[Q]], Program], d_secret: int = 2, prob_real: float = 0.4):
-        self.prob_real = prob_real
-        self.d_secret = d_secret
-        self.num_qubits = int(math.ceil(math.log2(self.d_secret)))
-        self.protocol = Program()
+    def __init__(self, secret_ansatz: Callable[[Q], Program], prob_real: float = 0.4):
         self.secret_ansatz = secret_ansatz
-        assert d_secret == 2, "Error: Can only currently process two dimensional secrets."
+        self.prob_real = prob_real
+        self.protocol = Program()
+        self.secret_revealed = False
 
-    def generate_qubit_map(self, num_bobs: int) -> QubitMap:
-        #num_qubits to represent the state, num_bobs - 1 for single particles
-        return [[QubitPlaceholder() for _ in range(num_qubits + num_bobs - 1)] for _ in range(num_bobs)]
+    def roll_matrix(self, matrix):
+        arr = np.array(matrix, dtype=np.object)
+        new_arr = np.zeros_like(arr)
+        for qubit in range(arr.shape[1]):
+            new_arr[:, qubit, :] = np.roll(arr[:, qubit, :], shift=qubit, axis=0)
+        return new_arr.tolist()
 
-    def generate_n_test_secrets(self, n: int, qubits: QubitMap, len_ansatz: int = 50) -> Program:
+    def tester_ansatz(self, qubit: Q, len_ansatz: int = 50) -> Program:
         angles = np.arange(0, np.pi, np.pi / 16)
         rotation_gates = [RX, RY, RZ]
         flip_gates = [X, Y, Z, H]
         pq = Program()
         for _ in range(len_ansatz):
-            qubit = np.random.choice(self.num_qubits)
             action = random()
-            if action < 0.4: 
+            if action < 0.4:
                 gate = np.random.choice(rotation_gates)
                 angle = np.random.choice(angles)
-                for i in range(n):
-                    pq += gate(angle, qubits[i][qubit])
+                pq += gate(angle, qubit)
             elif action < 1:
                 gate = np.random.choice(flip_gates)
-                for i in range(n):
-                    pq += gate(qubits[i][qubit])
+                pq += gate(qubit)
         return pq
 
-    def deal_states(self, num_bobs: int) -> QubitSourceMap:
-        should_reveal_secret = random()< self.prob_real
-        qubits = generate_qubit_map(num_bobs)
-        pq = Program()
-        if should_reveal_secret:
-            for i in range(num_bobs):
-                pq += self.secret_ansatz(qubits[i])
-        else:
-            pq += self.generate_n_test_secrets(num_bobs, qubits)
-        for qubits_b in qubits:
-            secret = qubits_b[:self.num_qubits]
-            single_particles = qubits_b[self.num_qubits:]
-            pq += iqft(*secret, self.num_qubits)
-            for single_particle in single_particles:
-                pq += CNOT(*secret, single_particle)
-                pq += qft(single_particle, 1)
-            pq += qft(*secret, self.num_qubits)
-        
-        output = [[] for _ in range(num_bobs)]
-        for i in range(len(qubits[0])):
-            for j in range(num_bobs):
-                source_idx = (j + i) % num_bobs
-                output[j].append((qubits[source_idx][i], source_idx))
-        return output
+    def generate_qubit_matrix(self, num_bobs: int) -> List[QubitShareList]:
+        return [[(QubitPlaceholder(), bob) for qubit in range(num_bobs)] for bob in range(num_bobs)]
+
+    def deal_shares(self, num_bobs: int) -> QubitShareMatrix:
+        q_mat = self.generate_qubit_matrix(num_bobs)
+        if random() < self.prob_real:
+            self.secret_revealed = True
+        for bob in range(num_bobs):
+            qubit_list = [q[0] for q in q_mat[bob]]
+            self.protocol += init_p(qubit_list[1:])
+            if self.secret_revealed:
+                self.protocol += self.secret_ansatz(qubit_list[0])
+            else:
+                self.protocol += self.tester_ansatz(qubit_list[0])
+            self.protocol += iqft(qubit_list[0])
+            self.protocol += entangle(qubit_list[0], qubit_list[1:])
+        return self.roll_matrix(q_mat)
+
+# class Alice:
+#     def __init__(self, secret_ansatz: Callable[[List[Q]], Program], prob_real: float = 0.4):
+#         self.prob_real = prob_real
+#         self.protocol = Program()
+#         self.secret_ansatz = secret_ansatz
+#
+#     def generate_qubit_map(self, num_bobs: int) -> QubitMap:
+#         #1 to represent the state, num_bobs - 1 for single particles
+#         return [[QubitPlaceholder() for _ in range(num_bobs)] for _ in range(num_bobs)]
+#
+#     def generate_n_test_secrets(self, n: int, qubits: QubitMap, len_ansatz: int = 50) -> Program:
+#         angles = np.arange(0, np.pi, np.pi / 16)
+#         rotation_gates = [RX, RY, RZ]
+#         flip_gates = [X, Y, Z, H]
+#         pq = Program()
+#         for _ in range(len_ansatz):
+#             qubit = 0
+#             action = random()
+#             if action < 0.4:
+#                 gate = np.random.choice(rotation_gates)
+#                 angle = np.random.choice(angles)
+#                 for i in range(n):
+#                     pq += gate(angle, qubits[i][qubit])
+#             elif action < 1:
+#                 gate = np.random.choice(flip_gates)
+#                 for i in range(n):
+#                     pq += gate(qubits[i][qubit])
+#         return pq
+#
+#     def deal_states(self, num_bobs: int) -> QubitSourceMap:
+#         should_reveal_secret = random()< self.prob_real
+#         qubits = generate_qubit_map(num_bobs)
+#         pq = Program()
+#         if should_reveal_secret:
+#             for i in range(num_bobs):
+#                 pq += self.secret_ansatz(qubits[i])
+#         else:
+#             pq += self.generate_n_test_secrets(num_bobs, qubits)
+#         for qubits_b in qubits:
+#             secret = qubits_b[:self.num_qubits]
+#             single_particles = qubits_b[self.num_qubits:]
+#             pq += iqft(*secret, self.num_qubits)
+#             for single_particle in single_particles:
+#                 pq += CNOT(*secret, single_particle)
+#                 pq += qft(single_particle, 1)
+#             pq += qft(*secret, self.num_qubits)
+#
+#         output = [[] for _ in range(num_bobs)]
+#         for i in range(len(qubits[0])):
+#             for j in range(num_bobs):
+#                 source_idx = (j + i) % num_bobs
+#                 output[j].append((qubits[source_idx][i], source_idx))
+#         return output
--- qft.py
+++ qft.py
@@ -1,7 +1,7 @@
 '''
-	Quantum Implementations of:
-		1. Fourier Transform
-		2. Inverse Fourier Transform
+    Quantum Implementations of:
+        1. Fourier Transform
+        2. Inverse Fourier Transform
 '''
 
 import numpy as np
@@ -15,14 +15,14 @@ from pyquil.gates import *
 ########################################
 
 def classical_qft(j, d):
-	k_vec = lambda k : np.array([0 for i in range(d) if i != k else 1])
-	omega = 2.0 * np.pi * 1j / d
-	return (1.0 / np.sqrt(d)) * np.sum([omega ** (k * j) * k_vec(k) for k in range(d)])
+    k_vec = lambda k : np.array([0 for i in range(d) if i != k else 1])
+    omega = 2.0 * np.pi * 1j / d
+    return (1.0 / np.sqrt(d)) * np.sum([omega ** (k * j) * k_vec(k) for k in range(d)])
 
 def classical_iqft(j, d):
-	k_vec = lambda k : np.array([0 for i in range(d) if i != k else 1])
-	omega = 2.0 * np.pi * 1j / d
-	return (1.0 / np.sqrt(d)) * np.sum([omega ** (-k * j) * k_vec(k) for k in range(d)])
+    k_vec = lambda k : np.array([0 for i in range(d) if i != k else 1])
+    omega = 2.0 * np.pi * 1j / d
+    return (1.0 / np.sqrt(d)) * np.sum([omega ** (-k * j) * k_vec(k) for k in range(d)])
 
 
 ########################################
@@ -31,45 +31,45 @@ def classical_iqft(j, d):
 
 # Reference: http://www-bcf.usc.edu/~tbrun/Course/lecture13.pdf (Page 10)
 def Rk_mat(k):
-	return np.array([[1, 0], [0, (2*np.pi*1j) / (2**k)]]) 
+    return np.array([[1, 0], [0, (2*np.pi*1j) / (2**k)]]) 
 
 def RK_gate(k):
-	# Get the Quil definition for the new gate
-	Rk_definition = DefGate("Rk", Rk_mat(k))
-	# Get the gate constructor
-	RK = Rk_definition.get_constructor()
-	return RK, Rk_definition
+    # Get the Quil definition for the new gate
+    Rk_definition = DefGate("Rk", Rk_mat(k))
+    # Get the gate constructor
+    RK = Rk_definition.get_constructor()
+    return RK, Rk_definition
 
 # Reference: https://courses.edx.org/c4x/BerkeleyX/CS191x/asset/chap5.pdf (Page 2)
 def QFT_mat(n):
-	omega = np.exp(2.0 * np.pi * 1j / n)
-	mat = np.ones((n, n))
-	for i in range(1, n):
-		for j in range(i, n):
-			mat[i][j] *= omega
-	return mat / math.sqrt(float(n))
+    omega = np.exp(2.0 * np.pi * 1j / n)
+    mat = np.ones((n, n))
+    for i in range(1, n):
+        for j in range(i, n):
+            mat[i][j] *= omega
+    return mat / math.sqrt(float(n))
 
 def QFT_gate(n):
-	# Get the Quil definition for the new gate
-	QFT_definition = DefGate("QFT", QFT_mat(k))
-	# Get the gate constructor
-	QFT = QFT_definition.get_constructor()
-	return QFT, QFT_definition
+    # Get the Quil definition for the new gate
+    QFT_definition = DefGate("QFT", QFT_mat(k))
+    # Get the gate constructor
+    QFT = QFT_definition.get_constructor()
+    return QFT, QFT_definition
 
 def IQFT_mat(n):
-	omega = np.exp(2.0 * np.pi * 1j / n)
-	mat = np.ones((n, n))
-	for i in range(1, n):
-		for j in range(i, n):
-			mat[i][j] /= omega
-	return mat / math.sqrt(float(n))
+    omega = np.exp(2.0 * np.pi * 1j / n)
+    mat = np.ones((n, n))
+    for i in range(1, n):
+        for j in range(i, n):
+            mat[i][j] /= omega
+    return mat / math.sqrt(float(n))
 
 def IQFT_gate(n):
-	# Get the Quil definition for the new gate
-	IQFT_definition = DefGate("IQFT", IQFT_mat(k))
-	# Get the gate constructor
-	IQFT = IQFT_definition.get_constructor()
-	return IQFT, IQFT_definition
+    # Get the Quil definition for the new gate
+    IQFT_definition = DefGate("IQFT", IQFT_mat(k))
+    # Get the gate constructor
+    IQFT = IQFT_definition.get_constructor()
+    return IQFT, IQFT_definition
 
 ########################################
 # Quantum Implemenations
@@ -77,17 +77,20 @@ def IQFT_gate(n):
 
 # Fourier Transform
 def qft(phi, n):
-	pq = Program()
-	QFT, QFT_definition = QFT_gate(n)
-	pq += QFT_definition
-	pq += QFT(phi)
-	return pq
+    pq = Program()
+    QFT, QFT_definition = QFT_gate(n)
+    pq += QFT_definition
+    if isinstance(phi, List):
+        pq += QFT(*phi)
+    else:
+        pq += QFT(phi)
+    return pq
 
 # Inverse Fourier Transform
 def iqft(phi, n):
-	pq = Program()
-	IQFT, IQFT_definition = IQFT_gate(n)
-	pq += IQFT_definition
-	pq += IQFT(phi)
-	return pq
+    pq = Program()
+    IQFT, IQFT_definition = IQFT_gate(n)
+    pq += IQFT_definition
+    pq += IQFT(phi)
+    return pq
 
