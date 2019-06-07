a541288de6771da1807c7560c65232e36993de89
Manan Rai <mananrai@stanford.edu>
2019-06-07 14:48:52 -0700

    Streamlined output and added tests

--- tests.py
+++ tests.py
@@ -17,19 +17,22 @@ def run(secret_ansatz: Callable[[Q], Program],
 		alice_prob_real: float = 0.4,
 		num_bobs: int = 2,
 		num_trials: int = 1,
-		verification_program: Callable[[np.array], bool] = lambda x: False):
+		verification_program: Callable[[np.array], bool] = lambda x: False,
+		consistent_cheating_bob: int = -1,
+		random_cheating_bob: int = -1):
 	alice = Alice(secret_ansatz, prob_real=alice_prob_real)
 	wf_sim = WavefunctionSimulator()
 	while not alice.secret_revealed:
 		protocol = Program()
 		bob_memories = protocol.declare('ro', 'BIT', num_bobs ** 2)
 		phi_dash_copies = alice.deal_shares(num_bobs)
-		print("Original secret:", wf_sim.wavefunction(address_qubits(secret_ansatz(alice.q_mat[0][0][0]))))
+		# print("Original secret:", wf_sim.wavefunction(address_qubits(secret_ansatz(alice.q_mat[0][0][0]))))
 
 		bobs = [Bob(shares=phi_dash_copies[curr_id],
 					memory=[bob_memories[curr_id * num_bobs + i] for i in range(num_bobs)],
 					self_id=curr_id,
-					is_cheater=(curr_id == 0))
+					is_consistent_cheater=(curr_id == consistent_cheating_bob),
+					is_random_cheater=(curr_id == random_cheating_bob))
 				for curr_id in range(num_bobs)]
 		
 		for idx, bob in enumerate(bobs):
@@ -54,30 +57,44 @@ def run(secret_ansatz: Callable[[Q], Program],
 		result = qc.run(ep)
 
 		alice.reset()
+		for bob in bobs:
+			bob.reset()
 
 		result = np.reshape(result, (num_bobs, num_bobs))
-		# print(result)
+		print(result)
 		if verification_program(result):
 			if alice.secret_revealed:
-				print("SECRET REVEALED")
+				print("SECRET REVEALED [{}]\n".format(wf_sim.wavefunction
+														(address_qubits
+															(secret_ansatz(alice.q_mat[0][0][0])))))
 			else:
 				print("RETRYING...")
 		else:
-			print("CHEATING DETECTED")
+			print("CHEATING DETECTED\n")
 			break
 
 	return result
 
 def tests():
-	secret_ansatz = lambda q: Program() + X(q)
-	# wf_sim = WavefunctionSimulator()
-	# print(wf_sim.wavefunction(address_qubits(pq)))
-	for num_bobs in range(2, 4):
-		def verification_program(results):
-			results = results[:, 1:]
-			return (results == results[0]).all()
+	secret_ansatzs = [
+		#lambda q: Program() + X(q),
+		lambda q: Program() + X(q) + H(q)
+	]
+	
+	for secret_ansatz in secret_ansatzs:
+		for num_bobs in range(2, 4):
+			def verification_program(results):
+				results = results[:, 1:]
+				return (results == results[0]).all()
 
-		result = np.array(run(secret_ansatz, num_bobs=num_bobs, verification_program=verification_program))
-
+			print("Running test with 1 Alice and {} Bobs, and no cheating Bobs:".format(num_bobs))
+			result1 = np.array(run(secret_ansatz, num_bobs=num_bobs, verification_program=verification_program))
+			for i in range(num_bobs):
+				print("Running test with 1 Alice and {} Bobs, such that Bob{} cheats with consistent program:".format(num_bobs, i))
+				result2 = np.array(run(secret_ansatz, num_bobs=num_bobs, verification_program=verification_program, consistent_cheating_bob=i))
+			for i in range(num_bobs):
+				print("Running test with 1 Alice and {} Bobs, such that Bob{} cheats with random program:".format(num_bobs, i))
+				result2 = np.array(run(secret_ansatz, num_bobs=num_bobs, verification_program=verification_program, random_cheating_bob=i))
+			
 if __name__ == "__main__":
 	tests()
